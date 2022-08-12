ceb59107b23523ce4e000c600d77ba2234e7ecce
Евгений Глебов <egoji@mail.ru>
2022-08-13 00:57:16 +0300

    test

--- src/pages/IntroPage.tsx
+++ src/pages/IntroPage.tsx
@@ -1,13 +1,53 @@
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+
+
 export function IntroPage() {
+    const title = useRef<HTMLParagraphElement>(null);
+    const description = useRef<HTMLParagraphElement>(null);
+    const button = useRef<HTMLButtonElement>(null);
+    const decoration01 = useRef<HTMLDivElement>(null);
+    const decoration02 = useRef<HTMLDivElement>(null);
+    const decoration03 = useRef<HTMLDivElement>(null);
+
+
+    useEffect(() => {
+        gsap.to(title.current!,  1.5, {opacity: 1, delay: .5})
+        gsap.to(description.current!,  1.5, {opacity: 1, delay: 1.5})
+        gsap.to(button.current!,.5, {opacity: 1, delay: 2.5})
+        gsap.to(decoration01.current!,10, {
+            scale: 1.2,
+            repeat: -1,
+            yoyo: true
+        })
+        gsap.to(decoration02.current!,10, {
+            scale: 1.3,
+            repeat: -1,
+            y:-100,
+            x:-50,
+            yoyo: true
+        })
+        gsap.to(decoration03.current!,10, {
+            scale: .6,
+            repeat: -1,
+            y:175,
+            x:-175,
+            yoyo: true
+        })
+    })
+
     return (
         <section className="intro">
             <div className="container intro__container">
                 <div className="intro__content">
-                    <h1 className="intro__title">Нынче ветрено и&nbsp;волны с&nbsp;перехлестом</h1>
-                    <p className="intro__description">Скоро осень, все изменится в&nbsp;округе</p>
-                    <button className="button intro__button">Продолжить</button>
+                    <h1 className="intro__title" ref={title}>Нынче ветрено и&nbsp;волны с&nbsp;перехлестом</h1>
+                    <p className="intro__description" ref={description}>Скоро осень, все изменится в&nbsp;округе</p>
+                    <button className="button intro__button" ref={button}>Продолжить</button>
                 </div>
             </div>
+            <div className="intro__decoration _01" ref={decoration01} />
+            <div className="intro__decoration _02" ref={decoration02} />
+            <div className="intro__decoration _03" ref={decoration03} />
         </section>
     );
 }
